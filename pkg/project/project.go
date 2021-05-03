87fde6af6d78083ab8f4a1a9c2bf4664f43d82e7
GitHub Action <action@github.com>
2021-05-03 07:39:09 +0000

    Bump version to 1.1.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "resource-police"
 	source      = "https://github.com/giantswarm/resource-police"
-	version     = "1.1.1"
+	version     = "1.1.2-dev"
 )
 
 func Description() string {
