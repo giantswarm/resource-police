73fa3f6618186916f1ade533396d44712fb6e27e
taylorbot <dev@giantswarm.io>
2023-11-29 14:53:59 +0000

    Release v1.3.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "resource-police"
 	source      = "https://github.com/giantswarm/resource-police"
-	version     = "1.2.1-dev"
+	version     = "1.3.0"
 )
 
 func Description() string {
