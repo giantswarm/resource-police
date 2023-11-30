890440325d52a2ce8e643f4eb496cbbf075120e5
taylorbot <dev@giantswarm.io>
2023-11-30 13:39:05 +0000

    Bump version to 1.3.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "resource-police"
 	source      = "https://github.com/giantswarm/resource-police"
-	version     = "1.3.1"
+	version     = "1.3.2-dev"
 )
 
 func Description() string {
